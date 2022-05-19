e1fda2947dc8a8474490a949f34513c3d85b60f1
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-05-18 22:34:37 -0300

    Create AccountController

--- src/main/java/utfpr/edu/projetofinal/controller/AccountController.java
+++ src/main/java/utfpr/edu/projetofinal/controller/AccountController.java
@@ -5,36 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import utfpr.edu.projetofinal.model.Account;
 import utfpr.edu.projetofinal.service.AccountService;
-import utfpr.edu.projetofinal.shared.GenericResponse;
+import utfpr.edu.projetofinal.service.CrudService;
 
 
 @RestController
 @RequestMapping("account")
-public class AccountController {
+public class AccountController extends CrudController<Account, Long>{
 
     @Autowired
     private AccountService accountService;
 
-    @PostMapping
-    GenericResponse createAccount(@RequestBody Account account){
-        accountService.save(account);
-        return new GenericResponse("Registro Salvo");
-    }
-
-    @GetMapping
-    GenericResponse findAllAccount(){
-        return new GenericResponse("Registros retornados: " + accountService.listAll());
-    }
-
-    @DeleteMapping("/{id}")
-    GenericResponse deleteAccount(@PathVariable long id){
-        accountService.deleteAccount(id);
-        return new GenericResponse("Registro Excluido");
-    }
-
-    @PutMapping("/{id}")
-    GenericResponse replaceAccount(@RequestBody Account acount, @PathVariable long id){
-        accountService.replaceAccount(acount,id);
-        return new GenericResponse("Registro Atualizado");
+    @Override
+    protected CrudService<Account, Long> getService() {
+        return this.accountService;
     }
 }
