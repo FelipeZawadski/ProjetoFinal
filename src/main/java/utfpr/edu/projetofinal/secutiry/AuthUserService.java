bc94f629df6faf03cfae521297ccba2afbdb46ea
FelipeZawadski <62263292+FelipeZawadski@users.noreply.github.com>
2022-05-18 22:35:15 -0300

    Authentication Create

---
+++ src/main/java/utfpr/edu/projetofinal/secutiry/AuthUserService.java
@@ -0,0 +1,26 @@
+package utfpr.edu.projetofinal.secutiry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import utfpr.edu.projetofinal.model.User;
+import utfpr.edu.projetofinal.repository.UserRepository;
+
+@Service
+public class AuthUserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
